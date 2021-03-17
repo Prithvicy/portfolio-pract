969c06c1dcb351e28c084cb5f3d7daea1d853e70
Prithvicy <prithvi40201@gmail.com>
2021-03-17 18:58:42 +0530

    3. services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import ServicesSection from "../components/ServicesSection";
 //importing page components here since like all part of the page
-import AboutSection from '../components/AboutSection'
+import AboutSection from "../components/AboutSection";
 
-const AboutUs =() => {
-    return (
-        <div>
-            <AboutSection/>
-        </div>
-    )
-}
+const AboutUs = () => {
+  return (
+    <div>
+      <AboutSection />
+      <ServicesSection />
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
